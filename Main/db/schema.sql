1be3a9d07650268c84c4609519b949fecaf7d5a6
suzakijun1 <dyong1997@gmail.com>
2023-04-29 00:25:00 -0400

    code

---
+++ Main/db/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+
+USE employees;
+
+CREATE TABLE department (
+
+);
+
+CREATE TABLE role (
+    -- what constraints?
+);
+
+CREATE TABLE employee (
+    -- what happens when something is deleted?
+);
---
+++ Main/index.js
@@ -0,0 +1,43 @@
+const { prompt } = require("inquirer");
+const logo = require("asciiart-logo");
+const db = require("./db");
+require("console.table");
+
+init();
+
+//Display logo text, load main prompts
+function init() {
+  const logoText = logo({ name: "Employee Manger" }).render();
+
+  console.log(logoText);
+
+  loadMainPrompts();
+}
+
+function LoadMainPrompts() {
+  prompt([
+    {
+      type: "list",
+      name: "choice",
+      message: "What would you like to do?",
+      choices: [
+        {
+          name: "View All Employees",
+          value: "VIEW_EMPLOYEES",
+        },
+        {
+          name: "View All Employees by Department",
+          value: "VIEW_EMPLOYEES_BY_DEPARTMENT",
+        },
+        {
+          name: "View All Employees by Manager",
+          value: "VIEW_EMPLOYEES_BY_MANAGER",
+        },
+        {
+          name: "Add Employee",
+          value: "ADD_EMPLOYEE",
+        },
+      ],
+    },
+  ]);
+}
